436c03887ab86a50e0bbe4bea35118dbec6439b6
provisionshohei <provisioninc@outlook.jp>
2021-08-18 17:18:16 +0900

    fifth commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo page'),
+      home: MyHomePage(title: 'Flutter Demo pageだってばよ'),
     );
   }
 }
